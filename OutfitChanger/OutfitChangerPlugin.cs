1bdba3af00b161b539195c65f7e683716aef486b
whichtwix <whichtwix@users.noreply.github.com>
2025-03-29 02:23:55 +0300

    initial commit

---
+++ OutfitChanger/OutfitChangerPlugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using UnityEngine;
+
+namespace OutfitChanger;
+
+[BepInAutoPlugin]
+[BepInProcess("Among Us.exe")]
+
+public partial class OutfitChangerPlugin : BasePlugin
+{
+    public Harmony Harmony { get; } = new(Id);
+
+    public static ConfigEntry<string> OutfitFolder { get; private set; }
+
+    public override void Load()
+    {
+        OutfitFolder = this.Config.Bind("General", "OutfitFolder", $"{Application.persistentDataPath}\\Outfits", "The folder where outfits are loaded from.");
+        Harmony.PatchAll();
+    }
+}
---
+++ OutfitChanger/Patches.cs
@@ -0,0 +1,91 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using HarmonyLib;
+using UnityEngine;
+
+namespace OutfitChanger;
+
+[HarmonyPatch]
+
+public class Patches
+{
+	static List<Customization> Outfits = [];
+
+	static int CurrentPage = 0;
+
+	[HarmonyPatch(typeof(PlayerCustomizationMenu), nameof(PlayerCustomizationMenu.Start))]
+	[HarmonyPostfix]
+
+	public static void Start()
+	{
+		Outfits = [];
+		var files = Directory.GetFiles(OutfitChangerPlugin.OutfitFolder.Value, "*.amogus");
+
+		foreach (var file in files)
+		{
+			var json = File.ReadAllText(file);
+
+			var root = JsonSerializer.Deserialize<Root>(json);
+
+			Outfits.Add(root.customization);
+		}
+	}
+
+	[HarmonyPatch(typeof(PlayerCustomizationMenu), nameof(PlayerCustomizationMenu.Update))]
+
+	public static void Postfix(PlayerCustomizationMenu __instance)
+	{
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			CurrentPage--;
+			CurrentPage = CurrentPage < 0 ? Outfits.Count - 1 : CurrentPage;
+
+			SetOutfit(Outfits[CurrentPage]);
+
+			__instance.PreviewArea.UpdateFromLocalPlayer(PlayerMaterial.MaskType.None);
+		}
+		else if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			CurrentPage++;
+			CurrentPage = CurrentPage > Outfits.Count - 1 ? 0 : CurrentPage;
+
+			SetOutfit(Outfits[CurrentPage]);
+
+			__instance.PreviewArea.UpdateFromLocalPlayer(PlayerMaterial.MaskType.None);
+		}
+	}
+
+	public static void SetOutfit(Customization outfit)
+	{
+		PlayerControl.LocalPlayer.CmdCheckColor((byte)outfit.colorID);
+		PlayerControl.LocalPlayer.RpcSetHat(outfit.hat ?? "hat_NoHat");
+		PlayerControl.LocalPlayer.RpcSetPet(outfit.pet ?? "pet_EmptyPet");
+		PlayerControl.LocalPlayer.RpcSetSkin(outfit.skin ?? "skin_None");
+		PlayerControl.LocalPlayer.RpcSetVisor(outfit.visor ?? "visor_EmptyVisor");
+		PlayerControl.LocalPlayer.RpcSetNamePlate(outfit.namePlate ?? "nameplate_NoPlate");
+		PlayerControl.LocalPlayer.CmdCheckName(outfit.name ?? "where name");
+	}
+}
+
+public class Customization
+{
+	public string name { get; set; }
+
+	public int colorID { get; set; }
+
+	public string pet { get; set; }
+
+	public string hat { get; set; }
+
+	public string skin { get; set; }
+
+	public string visor { get; set; }
+
+	public string namePlate { get; set; }
+}
+
+public class Root
+{
+	public Customization customization { get; set; }
+}
